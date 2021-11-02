49455cbdd0af51284b70eba57b54bb9cca45f869
nara.shin <joyshin93@gmail.com>
2021-11-02 15:26:19 +0900

    feat: create server-side and client-side socket

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,8 +9,18 @@ app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listening on http://localhost:3000");
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+//* We can run http and WebSocket server in the same server
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log(socket);
+});
+
+server.listen(3000, handleListen);
